90934f14eaaaadd39410755903847b3bcc17ce03
Wasut <wasut_pao@hotmail.com>
2017-01-02 19:31:44 +0700

    make fizzbuzz calculate

--- src/main/java/com/wasut/kata/FizzBuzz.java
+++ src/main/java/com/wasut/kata/FizzBuzz.java
@@ -8,6 +8,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class FizzBuzz {
     public String calculate(int number){
-        return "";
+        String result = "";
+
+        if(number % 3 == 0) {
+            result += "fizz";
+        }
+
+        if(number % 5 == 0){
+            result += "buzz";
+        }
+
+        return result.equals("") ? String.valueOf(number) : result;
     }
 }
