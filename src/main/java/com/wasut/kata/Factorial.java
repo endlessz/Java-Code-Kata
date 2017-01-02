13ed469a127e5de02f132148a3c81e1f4527a108
Wasut <wasut_pao@hotmail.com>
2017-01-02 20:11:23 +0700

    add calculateByLoop and more test case

--- src/main/java/com/wasut/kata/Factorial.java
+++ src/main/java/com/wasut/kata/Factorial.java
@@ -14,4 +14,14 @@ public class Factorial {
 
         return number * calculate(number - 1);
     }
+
+    public int calculateByLoop(int number){
+        int result = 1;
+
+        for (int i = 2; i <= number; i++){
+            result *= i;
+        }
+
+        return result;
+    }
 }
--- src/test/java/com/wasut/kata/FactorialTest.java
+++ src/test/java/com/wasut/kata/FactorialTest.java
@@ -23,5 +23,18 @@ public class FactorialTest {
         Assert.assertEquals(6, factorial.calculate(3));
         Assert.assertEquals(24, factorial.calculate(4));
         Assert.assertEquals(120, factorial.calculate(5));
+        Assert.assertEquals(720, factorial.calculate(6));
+        Assert.assertEquals(5040, factorial.calculate(7));
+    }
+
+    @Test
+    public void correctCalculateLoop(){
+        Assert.assertEquals(1, factorial.calculateByLoop(1));
+        Assert.assertEquals(2, factorial.calculateByLoop(2));
+        Assert.assertEquals(6, factorial.calculateByLoop(3));
+        Assert.assertEquals(24, factorial.calculateByLoop(4));
+        Assert.assertEquals(120, factorial.calculateByLoop(5));
+        Assert.assertEquals(720, factorial.calculateByLoop(6));
+        Assert.assertEquals(5040, factorial.calculateByLoop(7));
     }
 }
