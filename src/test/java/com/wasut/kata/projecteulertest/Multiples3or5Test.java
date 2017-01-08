310c4eb8c7ae1edf5093b7c1a19ab1923e385c12
Wasut <wasut_pao@hotmail.com>
2017-01-08 14:44:40 +0700

    add test to test number below 20, test for find answer of question

--- src/test/java/com/wasut/kata/projecteulertest/Multiples3or5Test.java
+++ src/test/java/com/wasut/kata/projecteulertest/Multiples3or5Test.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 /**
  * Created by Wasut on 8/1/2560.
  */
@@ -25,5 +27,20 @@ public class Multiples3or5Test {
         Assert.assertEquals(true, multiples3or5.isMultiple3or5(25));
         Assert.assertEquals(true, multiples3or5.isMultiple3or5(33));
         Assert.assertEquals(true, multiples3or5.isMultiple3or5(99));
+    }
+
+    @Test
+    public void multiple3or5Below20(){
+        List<Integer> list = multiples3or5.findMultiples3or5Below(20);
+
+        Assert.assertEquals(8, list.size());
+        Assert.assertEquals("[3, 5, 6, 9, 10, 12, 15, 18]", list.toString());
+    }
+
+    @Test
+    public void findAnswerBelow1000(){
+        List<Integer> list = multiples3or5.findMultiples3or5Below(1000);
+
+        System.out.println("Answer is " + multiples3or5.sumNumber(list));
     }
 }
